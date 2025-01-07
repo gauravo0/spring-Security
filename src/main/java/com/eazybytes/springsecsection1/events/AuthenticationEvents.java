e317bc924f71010395e6f6e7185d37c0d04425f7
gaurav <gwork1998kumar@gmail.com>
2025-01-07 19:42:27 +0530

    AuthenticationEntryPoint custom CustomBasicAuthenticationEntryPoint for api

---
+++ src/main/java/com/eazybytes/springsecsection1/events/AuthenticationEvents.java
@@ -0,0 +1,24 @@
+package com.eazybytes.springsecsection1.events;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AuthenticationEvents {
+
+    @EventListener
+    public void onSucess(AuthenticationSuccessEvent successEvent){
+        log.info("Login success for user: {}", successEvent.getAuthentication().getName());
+    }
+
+    @EventListener
+    public void onFailure(AbstractAuthenticationFailureEvent failureEvent){
+        log.error("Login failure for user: {} due to {}", failureEvent.getAuthentication().getName(),failureEvent.getException()
+                .getMessage());
+    }
+}
